47322287eb38321a0addea31eedd445eae343798
SMD111-git <owaissmd99@gmail.com>
2025-07-05 16:15:11 +0530

    add the npm packages cros and cookies and writing the app.use() in app.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { app } from "./app.js";
 dotenv.config({
     path:'./.env'
 })
-connectDB()
+connectDB() //as this is promise we do in next .then and .catch to show error in connectio or to start the sever with port and db
 .then(()=>{
     app.on("error",(error)=>{// thos another listner that handels error before the connection of db
         console.log("error",err);
         throw(err)   
     })
-    app.listen(process.env.PORT || 8000)
+    app.listen(process.env.PORT || 8000) //here the sever is statred at port with db connected  .
     console.log(`sever is run at :${process.env.PORT}`);
     
 })
