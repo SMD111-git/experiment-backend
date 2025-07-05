3af87f53a8b927165d563747837fbf464e612485
SMD111-git <owaissmd99@gmail.com>
2025-07-05 14:30:28 +0530

    hanndel error and promise in index.js root and import express in app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
 })
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{// thos another listner that handels error before the connection of db
+        console.log("error",err);
+        throw(err)   
+    })
+    app.listen(process.env.PORT || 8000)
+    console.log(`sever is run at :${process.env.PORT}`);
+    
+})
+.catch((err)=>{
+    console.log("ERROR DB CONNECTION FALIED", err);
+    
+})
