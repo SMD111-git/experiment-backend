5edb5ca115c9156b0c03784b8473ce92b0768334
SMD111-git <owaissmd99@gmail.com>
2025-07-10 20:21:56 +0530

    util commitng

--- src/utils/ApiRespone.js
+++ src/utils/ApiRespone.js
@@ -3,6 +3,6 @@ class Respone {
         this.statusCode=statusCode;
         this.data=data;
         this.message=message;
-        this.succes=statusCode<400;
+        this.succes=statusCode<400;//Sets the success flag to:true if the status code is less than 400 false otherwise
     }
 }
--- src/utils/Apierror.js
+++ src/utils/Apierror.js
@@ -4,7 +4,7 @@ class  ApiError extends Error {
         message="something went wrong",
         errors=[],
         stack="") {
-            super(message)
+            super(message) //this a vraible from parent classs error that this varaible is  sent into the class error which is already having the message handler defined init
             this.statusCode=statusCode
             this.data=null
             this.message=message
